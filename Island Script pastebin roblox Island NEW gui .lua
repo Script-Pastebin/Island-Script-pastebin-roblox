6b779aea379ede7b5a2b337b6861d490f961f185
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 04:01:03 +0100

    commit zero

---
+++ Island Script pastebin roblox Island NEW gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://system-exodus.com/scripts/Skyblock/Skyblock.lua",true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							The script for the Islands. It is very good and easy to understand. It has: Crop Farming, Ore&Item F
+						
+]]
